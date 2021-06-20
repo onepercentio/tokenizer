3e6035a1673006128d94380cbcfd994290bb29d6
Marcel Jackisch <jackisch@protonmail.com>
2021-06-20 23:00:13 +0200

    changes to batchCollection

--- packages/hardhat/contracts/BatchCollection.sol
+++ packages/hardhat/contracts/BatchCollection.sol
@@ -7,21 +7,53 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "hardhat/console.sol";
 
-contract BatchContract is ERC721, Ownable {
+contract BatchCollection is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    using SafeMath for uint256;
+
     event BatchMinted(address sender, string purpose);
 
-    constructor() ERC721("ClaimCollection", "v0.1-Claim") {}
-
     address public contractRegistry;
+    Counters.Counter private _tokenIds;
+
+    // WIP: The fields and naming is subject to change
+    struct NFTData {
+        string projectName;
+        string vintage;
+        string symbol;
+        uint256 quantity;
+        bool approved;
+    }
+
+    mapping (uint256 => NFTData) public nftList;
+
+    constructor() ERC721("ClaimCollection", "v0.1-Claim") {}
 
     //onlyOwner
     function setContractRegistry(address _address) public onlyOwner {
         contractRegistry = _address;
     }
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    using SafeMath for uint256;
+
+    function getNftData(uint256 tokenId) public view returns (string memory) {
+        return nftList[tokenId].vintage;
+    }
+
+    // here for debugging/mock purposes. safeTransferFrom(...) is error prone with ethers.js
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+        
+        console.log("\n--------------");
+        console.log("DEBUG sol: called transferFrom(): msg.sender:", msg.sender);
+        console.log("DEBUG sol:", from, to, tokenId);
+        console.log("DEBUG sol: address of CO2KenNFTCollection", address(this));
+        
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+
+        // _transfer(from, to, tokenId);
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+
 
 
     function ownerBalanceOf(address owner) public view returns (uint256) {
@@ -30,6 +62,7 @@ contract BatchContract is ERC721, Ownable {
 
         return (balance);
     }
+
 
     function mintBatch(address to, string memory tokenURI)
         public
@@ -48,7 +81,14 @@ contract BatchContract is ERC721, Ownable {
         uint256 newItemId = _tokenIds.current();
         console.log("minting to ", to);
         console.log("newItemId is ", newItemId);
-        _mint(to, newItemId);
+        _safeMint(to, newItemId);
+
+        // // This needs different function parameters
+        // nftList[newItemId].projectName = projectName;
+        // nftList[newItemId].vintage = vintage;
+        // nftList[newItemId].symbol = symbol;
+        // nftList[newItemId].quantity = CO2tons;
+
         // _setTokenURI(newItemId, tokenURI);
         emit BatchMinted(to, tokenURI);
         return newItemId;
