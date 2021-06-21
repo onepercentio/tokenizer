9e6d1002236b334493469af3a6d9b2af150d79b5
Marcel Jackisch <jackisch@protonmail.com>
2021-06-21 18:09:47 +0200

    naming changes in contractRegistry

--- packages/hardhat/contracts/ContractRegistry.sol
+++ packages/hardhat/contracts/ContractRegistry.sol
@@ -3,16 +3,16 @@ pragma solidity >=0.6.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ContractRegistry is Ownable {
-    address public batchAddress;
-    address public projectAddress;
+    address public batchCollectionAddress;
+    address public projectCollectionAddress;
     address public projectFactoryAddress;
 
-    function setProjectAddress(address _address) public onlyOwner {
-        projectAddress = _address;
+    function setProjectCollectionAddress(address _address) public onlyOwner {
+        projectCollectionAddress = _address;
     }
     
-    function setBatchAddress(address _address) public onlyOwner {
-        batchAddress = _address;
+    function setBatchCollectionAddress(address _address) public onlyOwner {
+        batchCollectionAddress = _address;
     }
 
     function setProjectFactoryAddress(address _address) public onlyOwner {
