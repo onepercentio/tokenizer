3d54c38f202e8b081783ed8d1e1ed97ba99238b0
James Farrell <info@jamespfarrell.com>
2021-06-22 13:21:17 +0200

    Smart contracts to get batches owned by the user, not returning all data to UI yet

--- packages/react-app/src/views/AccountPage.jsx
+++ packages/react-app/src/views/AccountPage.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Heading, Flex, Box, SimpleGrid } from "@chakra-ui/react";
 import { useContractReader, useEventListener, useResolveName } from "../hooks";
-import {AppContainer, Container} from "./styles/Tokenize"
+import { AppContainer, Container } from "./styles/Tokenize";
 
-export default function AccountPage({address, mainnetProvider, userProvider, localProvider, yourLocalBalance, price, tx, readContracts, writeContracts }) {
-  
-  const ownerBalanceOf = useContractReader(readContracts, "BatchCollection", "ownerBalanceOf", [address])
-  const getNFTs = useContractReader(readContracts, "BatchCollection", "getNftData", [2])
-  console.log("getNFTS are:", getNFTs)
+export default function AccountPage({
+  address,
+  mainnetProvider,
+  userProvider,
+  localProvider,
+  yourLocalBalance,
+  price,
+  tx,
+  readContracts,
+  writeContracts,
+}) {
+  const ownerBalanceOf = useContractReader(readContracts, "BatchCollection", "ownerBalanceOf", [address]);
+  const userBatches = useContractReader(readContracts, "BatchCollection", "tokensOfOwner", [address]);
+  console.log("userBatches are:", userBatches);
 
-return (
+  return (
     <div>
       <Container>
         <AppContainer>
-        <Heading>You have {ownerBalanceOf !== undefined ? parseInt(ownerBalanceOf._hex, 16) : 0} NFTs</Heading>
-        <SimpleGrid>
-          {/* {NFTarray.map((NFT) => (<Box>{NFT}</Box>))} */}
-        </SimpleGrid>
+          <Heading>You have {ownerBalanceOf !== undefined ? parseInt(ownerBalanceOf._hex, 16) : 0} NFTs</Heading>
+          <SimpleGrid>
+            {userBatches && userBatches.length ? userBatches.map(batch => <Box>{JSON.stringify(batch)}</Box>) : null}
+          </SimpleGrid>
         </AppContainer>
       </Container>
     </div>
