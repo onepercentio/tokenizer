c339843ed5e34748b6cc05d169c3e3a5eb1ca8c0
James Farrell <info@jamespfarrell.com>
2021-06-25 14:27:45 +0200

    Change kovan details back to localhost

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -61,8 +61,8 @@ import ExampleUI from "./views/ExampleUI";
 */
 
 /// 📡 What chain are your contracts deployed to?
-// const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
-const targetNetwork = NETWORKS.kovan; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+// const targetNetwork = NETWORKS.kovan; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
