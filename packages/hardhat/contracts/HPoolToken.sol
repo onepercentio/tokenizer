b1963f1165e20d69a03c2dcf497520a439131e2b
Marcel Jackisch <jackisch@protonmail.com>
2021-07-03 16:26:44 +0200

    basic approve/deposit

--- packages/hardhat/contracts/HPoolToken.sol
+++ packages/hardhat/contracts/HPoolToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol"; // dev & testing
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
@@ -15,23 +16,49 @@ contract HPoolToken is Context, ERC20, Ownable {
     // Initial supply = 0
     uint256 public _totalSupply = 0;
 
-    struct AttributeSet {
+    // Describes the allowe attributes as arrays per set
+    struct  AttributeSet {
         string[] vintages;
         string[] region;
         string[] standard;
         string[] pids;
     }
 
-    function addAttributeSet(
-        string[] memory vintages,
-        string[] memory region,
-        string[] memory standard,
-        string[] memory pids
-        ) public onlyOwner {
-
-    }
+    // All allowed sets
+    AttributeSet[] allowedSets;
 
     constructor(string memory name_, string memory _symbol) ERC20(name_, _symbol) {}
+
+    // Function to create new AttributeSets and add them to allowedSets
+    function addAttributeSet(
+        string[] memory _vintages,
+        string[] memory _regions,
+        string[] memory _standards,
+        string[] memory _pids
+        ) public onlyOwner {
+        AttributeSet memory set;
+
+        set.vintages = _vintages;
+        set.vintages = _regions;
+        set.vintages = _standards;
+        set.vintages = _pids;
+
+        allowedSets.push(set);
+    }
+
+
+    // Wrapper for external functions
+    function approveExternal(address erc20Addr, address spender, uint amount) public {
+        IERC20(erc20Addr).approve(spender, amount);
+    } 
+
+    function deposit(address erc20Addr, uint amount) public {
+        // transfers token from sender (current owner) to this contract
+        IERC20(erc20Addr).transferFrom(msg.sender, address(this), amount);
+
+        // mints pool/index token to prev. owner(sender)
+        _mint(msg.sender, amount);
+    }
 
     // Checks if an ERC20 contract is whitelisted
     function checkWhiteListed(address collection) internal view onlyOwner returns (bool) {
