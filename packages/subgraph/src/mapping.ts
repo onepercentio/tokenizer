f93533f90ae4cd1467a866d23e0824b7885ba829
James Farrell <info@jamespfarrell.com>
2021-07-12 17:57:12 +0200

    The graph for the batch

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,34 +1,63 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
-  YourContract,
-  SetPurpose
-} from "../generated/YourContract/YourContract"
-import { Purpose, Sender } from "../generated/schema"
+  BatchMinted
+} from "../generated/BatchCollection/BatchCollection"
+import { Batch, Purpose, Sender } from "../generated/schema"
 
-export function handleSetPurpose(event: SetPurpose): void {
-
+export function handleMintBatch(event: BatchMinted): void {
+  
+  let batch = new Batch(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  
   let senderString = event.params.sender.toHexString()
+  let minter = Sender.load(senderString)
 
-  let sender = Sender.load(senderString)
-
-  if (sender == null) {
-    sender = new Sender(senderString)
-    sender.address = event.params.sender
-    sender.createdAt = event.block.timestamp
-    sender.purposeCount = BigInt.fromI32(1)
-  }
-  else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
+  if (minter == null) {
+    minter = new Sender(senderString)
+    minter.address = event.params.sender
+    minter.createdAt = event.block.timestamp
+    minter.batchCount = BigInt.fromI32(1)
+  } else {
+    minter.batchCount = minter.batchCount.plus(BigInt.fromI32(1))
   }
 
-  let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
 
-  purpose.purpose = event.params.purpose
-  purpose.sender = senderString
-  purpose.createdAt = event.block.timestamp
-  purpose.transactionHash = event.transaction.hash.toHex()
+  //Not sure the even needs to 
+  //batch.address = event.params.address
+  //batch.address = event.params.sender
+  batch.minter = senderString
+  batch.status = "initialized"
+  batch.owner = event.params.sender
+  //batch.createdAt = event.block.timestamp
+  //batch.transactionHash = event.transaction.hash.toHex()
 
-  purpose.save()
-  sender.save()
-
+  batch.save()
+  minter.save()
 }
+
+// export function handleSetPurpose(event: SetPurpose): void {
+
+//   let senderString = event.params.sender.toHexString()
+
+//   let sender = Sender.load(senderString)
+
+//   if (sender == null) {
+//     sender = new Sender(senderString)
+//     sender.address = event.params.sender
+//     sender.createdAt = event.block.timestamp
+//     sender.purposeCount = BigInt.fromI32(1)
+//   }
+//   else {
+//     sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
+//   }
+
+//   let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+
+//   purpose.purpose = event.params.purpose
+//   purpose.sender = senderString
+//   purpose.createdAt = event.block.timestamp
+//   purpose.transactionHash = event.transaction.hash.toHex()
+
+//   purpose.save()
+//   sender.save()
+
+// }
